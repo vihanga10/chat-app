2a0f0c563c1f78e5afe2f27bfdc321f3f8a0e756
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-06 22:41:54 +0530

    add route path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <div>
-
+    <div className="bg-[url('./src/assets/bgImage.svg')]bg-contain">
+     <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/> 
+      <Route path='/profile' element={<ProfilePage/>}/>
+     </Routes>
     </div>
   )
 }
