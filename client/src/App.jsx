e40c2e7a48e43253c3fac11e99cc652bffde6e85
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-08 11:13:06 +0530

    add background image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')]bg-contain">
+    <div className="bg-[url('./src/assets/bgImage.jpeg')]bg-contain">
      <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/login' element={<LoginPage/>}/> 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const HomePage = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div 
+      className='border w-full h-screen sm:px-[15%] sm:py-[5%]'
+      style={{
+        backgroundImage: `url(${assets.bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
+        
     </div>
   )
 }
