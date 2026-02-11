8023d2a599714c0dd0e3f87ca3dfa42c29bcc975
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-11 22:44:47 +0530

    Add user profiles

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 
@@ -33,6 +33,27 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                 <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1 py-[12px] pl-[12px]' placeholder='Search User...' />
               </div>
         </div>
+          <div className='flex flex-col'>
+            {userDummyData.map((user,index)=>(
+              <div className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+                <img src={user?.profilePic || assets.avatar_icon} alt="" 
+                className='w-[35px] aspect-[1/1] rounded-full'/>
+                <div className='flex flex-col leading-5'>
+                  <p>{user.fullName}</p>
+                  {
+                    index<3
+                    ? <span className='text-green-400 text-xs'>Online</span>
+                    : <span className='text-neutral-400 text-xs'>Offline</span>
+                    
+                  }
+
+                </div>
+                {index >2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+              </div>
+            ) )}
+
+          </div>
+
     </div>
   )
 }
