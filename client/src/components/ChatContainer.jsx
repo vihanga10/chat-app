d459ff5ff18ff53a83eeab7476104dc4754e6309
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-12 12:12:44 +0530

    Update chat container

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const ChatContainer = () => {
+const ChatContainer = ({selectedUser,setSelectedUser}) => {
   return (
-    <div>
-        <h1>ChatContainer</h1>
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+        <div className='flex items-center gap-3 pу-3 mx-4 border-b border-stone-500'>
+          <img src={assets.profile_martin} alt="" className="w-8 rounded-full"/>
+          <p className='flex-1 text-lg text-white flex items-center gap-2'>Martin Johnson
+            <span className="w-2 h-2 rounded-full bg-green-500"></span>
+          </p>
+            <img onClick={()=> setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
+            <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5'/>
+        </div>
     </div>
   )
 }
