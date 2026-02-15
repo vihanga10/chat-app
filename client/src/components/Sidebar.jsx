1624b5d2a7e8836944c61b07b666f7c760d89eb9
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-15 11:11:33 +0530

    Update sidebar tailwind css

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,9 +8,9 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser? "max-md:hidden" : ''}`}>
+    <div className={`bg-[#3A5D7A]/20 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser? "max-md:hidden" : ''}`}>
         <div className='pb-5'>
-            <div className='flex justify-between items-center -mt-5'>
+            <div className='flex justify-between items-center -mt-5 pl-0'>
                 <img src={assets.logo} alt="logo" className='max-w-40' />
                 <div className="relative py-2 group inline-block">
                 <img
@@ -27,10 +27,10 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                 </div>
 
             </div>
-              <div className='bg-[#03788D] rounded-full flex items-center gap-5 pу-3 px-5
+              <div className='bg-[#345BFF] rounded-full flex items-center gap-5 pу-3 px-5 ml-4
                mt -5'>
                 <img src={assets.search_icon} alt="Search" className='w-3' />
-                <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1 py-[12px] pl-[12px]' placeholder='Search User...' />
+                <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#F0F0F0] flex-1 py-[12px] pl-[12px]' placeholder='Search User...' />
               </div>
         </div>
           <div className='flex flex-col'>
@@ -39,16 +39,16 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
               key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
                 <div className='relative'>
                   <img src={user?.profilePic || assets.avatar_icon} alt="" 
-                  className='w-[35px] aspect-[1/1] rounded-full'/>
+                  className='w-[35px] aspect-[1/1] rounded-full shadow-lg'/>
                   <div 
                     className='absolute -bottom-0.5 -right-0.5 w-2.5 h-2.5 rounded-full border-1 border-white' 
                     style={{backgroundColor: index < 3 ? '#348C56' : '#545454'}}
                   ></div>
                 </div>
                 <div className='flex flex-col leading-5'>
-                  <p>{user.fullName}</p>
+                  <p className='text-gray-100 font-regular'>{user.fullName}</p>
                 </div>
-                {index >2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-[#396D99]'>{index}</p>}
+                {index >2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-[#345BFF]'>{index}</p>}
               </div>
             ) )}
 
