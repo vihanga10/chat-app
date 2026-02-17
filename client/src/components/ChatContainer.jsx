e5ed6d914803b53b8007c953b50e738b4fbeb1dd
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-02-17 15:31:45 +0530

    Update useRef

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
 
 const ChatContainer = ({selectedUser,setSelectedUser}) => {
